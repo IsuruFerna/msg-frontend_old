cb17b206ce582d2754b53e1576beb77ba187d674
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-10-06 16:58:47 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Badge from "react-bootstrap/Badge";
+import Button from "react-bootstrap/Button";
+import { Container, Row, Col } from "react-bootstrap";
+import ListGroup from "react-bootstrap/ListGroup";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div>
+         <Container>
+            <Row>
+               <Col md={4}>
+                  Friends
+                  <ListGroup as="ol" numbered>
+                     <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
+                     <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
+                     <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
+                  </ListGroup>
+               </Col>
+               <Col className="vh-100" md={8}>
+                  Chat
+                  <Row className="h-75">Chat space</Row>
+                  <Row className="h-25">Chat box</Row>
+               </Col>
+            </Row>
+         </Container>
+      </div>
+   );
 }
 
 export default App;
